ab1ccf18b3e4d3282b91c6e99971ab22a990d416
Jeebe Diop <jeebe.diop@gmail.com>
2020-02-27 00:13:28 -0600

    Update script.js
    fixed javascript for my form

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
-String.prototype.contains = function(str) { return this.indexOf(str) != -1; };
+function ProfanityCheck() {
+  String.prototype.contains = function(str) { return this.indexOf(str) != -1; };
 
-var profanities = new Array("ass", "cunt", "pope", "shit", "fuck", "rape");
+  var profanities = new Array("ass", "cunt", "pope", "shit", "fuck", "rape");
 
-var containsProfanity = function(text){
-    var returnVal = false;
-    for (var i = 0; i < profanities.length; i++) {
-        if(text.toLowerCase().contains(profanities[i].toLowerCase())){
-            returnVal = true;
-            break;
-        }
-    }
-    return returnVal;
-}
+  var containsProfanity = function(text){
+      var returnVal = false;
+      for (var i = 0; i < profanities.length; i++) {
+          if(text.toLowerCase().contains(profanities[i].toLowerCase())){
+              returnVal = true;
+              break;
+          }
+      }
+      return returnVal;
+  }
 
-var myText = $('#IGN').html();
+  var myText = $('#IGN').html();
 
-if(containsProfanity(myText)){
-    alert('That username contains profanity. Please try another username.');
-}
-else{
-    alert('Form submitted.');
+  if(containsProfanity(myText)){
+      alert('That username contains profanity. Please try another username.');
+  }
+  else{
+      alert('Form submitted.');
+  }
 }
