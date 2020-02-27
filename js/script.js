aac77b646238bb0794bdcb5a72993c960086c743
Jeebe Diop <jeebe.diop@gmail.com>
2020-02-27 00:33:01 -0600

    Update script.js
    fixed javascript final

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function ProfanityCheck() {
   String.prototype.contains = function(str) { return this.indexOf(str) != -1; };
 
-  var profanities = new Array("ass", "cunt", "pope", "shit", "fuck", "rape");
+  var profanities = new Array("ass", "cunt", "pope", "shit", "fuck", "rape", "faggot", "retard");
 
   var containsProfanity = function(text){
       var returnVal = false;
@@ -14,12 +14,18 @@ function ProfanityCheck() {
       return returnVal;
   }
 
-  var myText = $('#IGN').html();
+  var myText = document.getElementById('ign').value;
 
   if(containsProfanity(myText)){
       alert('That username contains profanity. Please try another username.');
   }
   else{
+    if((document.getElementById('fname').value && document.getElementById('lname').value && document.getElementById('email').value
+    && document.getElementById('password').value && document.getElementById('ign').value) != '' ) {
       alert('Form submitted.');
+    }
+    else {
+      alert('Please fill out the remaining fields');
+    }
   }
 }
